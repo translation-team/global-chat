54fe71296c286f97186794ee639396bc0571f6c7
Jaeeun <pjcson8@naver.com>
2022-12-19 19:01:29 +0900

    test용 기능 삭제

--- app/test/test_axios/testapp.py
+++ app/test/test_axios/testapp.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
+
+from database.database import SessionLocal, ENGINE
+from database.model import Base, MessageHistory, OriginMessage
+from database.schema import MessageHistorySchma, OriginCreate, HistoryCreate
+
+import re
 
 app = FastAPI()
 app.mount('/static',StaticFiles(directory='statics'),name='static')
@@ -22,18 +31,70 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# db 연결
+Base.metadata.create_all(ENGINE)
 
-class User(BaseModel):
-    firstname:str
-    age:int
+def get_db():
+    db = SessionLocal()
 
-# axios 테스트
-@app.post('/api/user')
-async def get_user(user:User):
-    print(user)
+    try:
+        yield db
+    except Exception as e:
+        pass
+    finally:
+        db.close()
 
-    return {'hello':'world'}
-
-@app.get('/')
+#화면에 html 파일 출력 & 채팅 정보 전체 읽기.
+@app.get('/api/v1/chat')
 async def index():
-    return FileResponse('templates/index.html')
+    
+    return FileResponse('templates/index2.html') 
+
+
+# DB(origin_message, Message_history)에 저장
+@app.post('/api/v1/chat')
+async def get_messsage(msg:MessageHistorySchma, db=Depends(get_db)):
+    
+    # 한글/영어 구분
+    word=msg.contents
+    
+    reg = re.compile(r'[a-zA-Z]')
+    ori_Lang='ko'
+    if reg.match(word):
+        ori_Lang='en'
+    else:
+        ori_Lang='ko'
+        
+    # (origin_message) DB 저장
+    message = OriginCreate(
+        body=msg.contents, 
+        lang=ori_Lang
+    )
+    
+    origin_message = OriginMessage(**message.dict())
+    
+    try:
+        db.add(origin_message)
+        db.commit()
+    except Exception as e:
+        print("에러")
+        return {'fail':'실패'}
+
+    #(message_history) DB 저장
+    history=HistoryCreate(
+        room_id=msg.room_id,
+        from_id=msg.from_id,
+        to_id=msg.to_id,
+        origin_id=origin_message.id #foreign key
+    )
+    
+    message_history=MessageHistory(**history.dict())
+
+    try:
+        db.add(message_history)
+        db.commit()
+        return {'success':'성공'}
+    except Exception as e:
+        print("에러")
+        return {'fail':'실패2'}
+    
